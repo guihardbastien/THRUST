3917e8aa13c9767d41120c2f3b42b4571e4d5009
bguihard <bastien.guihard@superpitch.fr>
2020-06-14 18:44:01 +0200

    feat: loader

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -1,6 +1,6 @@
 
 <template>
-   <div class="container max-w-xs rounded-lg overflow-hidden shadow-lg my-2 bg-white">
+   <div class="container max-w-xs rounded-lg overflow-hidden shadow-lg my-2 bg-white p-4">
       <slot></slot>
    </div>
 </template>
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+declare module 'vue-loaders/dist/loaders/ball-beat';
