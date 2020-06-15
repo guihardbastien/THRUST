652002b7f86ad8694df5f31853149c8caa631500
bguihard <bastien.guihard@superpitch.fr>
2020-06-15 16:21:23 +0200

    fix: card

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="container max-w-xs rounded-lg overflow-hidden shadow-lg my-2 bg-white p-4">
+  <div class="container rounded-lg overflow-hidden shadow-lg my-2 bg-white p-4">
     <slot></slot>
   </div>
 </template>
